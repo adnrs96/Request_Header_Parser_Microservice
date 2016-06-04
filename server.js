0902f40e032c69f9abc214627468f1cdb768914f
Aditya Bansal <adi.bansal241996@gmail.com>
2016-06-04 19:51:07 +0530

    Added the parsers for ip, language and ua

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var ip = require('ip');
+var acceptLanguage = require('accept-language');
+var UAParser = require('ua-parser-js');
+var app = express();
+var parser = new UAParser();
+
+app.set('port', (process.env.PORT || 5000));
+app.use('/whoami',function(req,res){
+	var ua = req.headers['user-agent'];
+	var os = parser.setUA(ua).getOS()["name"];
+	var language = acceptLanguage.parse(req.headers["accept-language"])[0].value;
+	res.json(ip.address());
+});
+app.use('/',function(req,res){
+	res.sendFile(__dirname+'/index.html');
+});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
