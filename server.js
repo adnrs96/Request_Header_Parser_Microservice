3d917a5b10449dd3ba6a4cd957b2b227347f1fed
Aditya Bansal <adi.bansal241996@gmail.com>
2016-06-04 19:54:32 +0530

    Corrected variable name

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use('/whoami',function(req,res){
 	var ua = req.headers['user-agent'];
 	var os = parser.setUA(ua).getOS()["name"];
 	var language = acceptLanguage.parse(req.headers["accept-language"])[0].value;
-	var ip = ip.address();
-	var rhpm = {"ipaddress":ip,"language":language,"software":os};
+	var ipa = ip.address();
+	var rhpm = {"ipaddress":ipa,"language":language,"software":os};
 	res.json(rhpm);
 });
 app.use('/',function(req,res){
